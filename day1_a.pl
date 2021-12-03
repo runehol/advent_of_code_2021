4ca23567e6919d3f33d48f52c9da7fb7b2bb6e32
Rune Holm <rune.holm@arm.com>
2021-12-03 16:25:24 +0000

    solved day 1

---
+++ day1_a.pl
@@ -0,0 +1,37 @@
+:- use_module(library(clpfd)).
+
+
+file_to_numbers(File, Numbers) :-
+    setup_call_cleanup(open(File, read, In),
+        stream_lines(In, Numbers),
+        close(In)).
+
+read_line(_, end_of_file, []).
+
+read_line(In, Line, [Num|Numbers]) :-
+	Line \== end_of_file,
+	number_string(Num, Line),
+	stream_lines(In, Numbers).
+
+
+
+stream_lines(In, Numbers) :-
+    read_line_to_string(In, Line),
+	read_line(In, Line, Numbers).
+
+count_up_numbers(Prev, [Num|Numbers], Count, Res) :-
+	Num #> Prev,
+	Count1 #= Count + 1,
+	count_up_numbers(Num, Numbers, Count1, Res).
+
+count_up_numbers(Prev, [Num|Numbers], Count, Res) :-
+	Num #=< Prev,
+	count_up_numbers(Num, Numbers, Count, Res).
+
+count_up_numbers(_, [], Count, Count).
+
+main :-
+	file_to_numbers("day1_input.txt", Numbers),
+	Numbers = [N|Rest],
+	count_up_numbers(N, Rest, 0, Count),
+	writeln(Count).
