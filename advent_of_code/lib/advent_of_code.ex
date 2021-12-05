ee4f806687c1cc0e1188a8c21de6cf7075a9e4ce
Rune Holm <rune.holm@arm.com>
2021-12-05 12:08:42 +0000

    better refactored code

--- advent_of_code/lib/advent_of_code.ex
+++ advent_of_code/lib/advent_of_code.ex
@@ -4,53 +4,56 @@ defmodule AdventOfCode do
   Documentation for `AdventOfCode`.
   """
 
-  def transpose(data) do
-    data
-    |> Stream.zip
-    |> Stream.map(&Tuple.to_list/1)
-  end
 
   def parse_contents(contents) do
     contents
     |> String.split("\n", trim: true)
-    |> Enum.map(&to_charlist/1)
-    |> Enum.map(fn(lst) -> (Enum.map(lst, &(&1 - 48))) end)
-    |> transpose()
+    |> Enum.map(&(&1 |> String.to_charlist() |> List.to_tuple()))
   end
 
   def read_data(fname) do
     case File.read(fname) do
       {:ok, contents} -> contents |> parse_contents()
-      end
-  end
-
-  def most_common(lst) do
-    if Enum.sum(lst) >= length(lst)/2 do
-      1
-    else
-      0
     end
   end
 
-  def bit_list_to_integer(lst) do
+
+  def most_common_for_pos(lst, idx) do
+    one_count = Enum.count(lst, &(elem(&1, idx) == ?1))
+    if one_count >= length(lst)/2, do: ?1, else: ?0
+  end
+
+  def most_common(lst) do
+    [first | _] = lst
+    n_bits = tuple_size(first)
+    most_common_as_list =
+      for pos <- 0..(n_bits-1) do
+        most_common_for_pos(lst, pos)
+      end
+    most_common_as_list
+  end
+
+  def list_to_integer(lst) do
     lst
-    |> Enum.map(&(&1 + 48))
     |> List.to_integer(2)
   end
 
 
+  def invert(lst) do
+    lst |> Enum.map(&(if &1 == ?0, do: ?1, else: ?0))
+  end
 
   def day3_a do
     data = read_data("day3_input.txt")
-    most_common_bits = data
-      |> Enum.map(&most_common/1)
 
-    least_common_bits = most_common_bits
-      |> Enum.map(&(1 - &1))
-    gamma = bit_list_to_integer(most_common_bits)
-    epsilon = bit_list_to_integer(least_common_bits)
+
+    most_common_bits = most_common(data)
+
+    least_common_bits = invert(most_common_bits)
+    gamma = list_to_integer(most_common_bits)
+    epsilon = list_to_integer(least_common_bits)
     gamma_epsilon = gamma * epsilon
     IO.puts(gamma_epsilon)
-
   end
+
 end
