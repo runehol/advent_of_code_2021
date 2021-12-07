5be51c3ebaa5889df202734c95d33ad659a6e69a
Rune Holm <rune.holm@arm.com>
2021-12-07 10:32:37 +0000

    solved day 7 puzzle

---
+++ day7.exs
@@ -0,0 +1,63 @@
+#! /usr/bin/env elixir
+
+defmodule Day7 do
+
+  def read_data(fname) do
+    {:ok, contents} = File.read(fname)
+    contents
+    |> String.trim()
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+
+  def linear_cost_for_pos(subs, pos) do
+    subs
+    |> Enum.map(&(abs(&1-pos)))
+    |> Enum.sum()
+  end
+
+  def sum_of_integers(n) do
+    div(n*(n+1), 2)
+  end
+
+  def sum_cost_for_pos(subs, pos) do
+    subs
+    |> Enum.map(&(sum_of_integers(abs(&1-pos))))
+    |> Enum.sum
+  end
+
+  def search(pos, cost, subs, cost_fun) do
+    next_cost = cost_fun.(subs, pos+1)
+    gradient = next_cost - cost
+    if gradient > 0 do
+      cost #if the gradient is positive, we're at the minimum and can stop
+    else
+      search(pos+1, next_cost, subs, cost_fun)
+    end
+  end
+
+  def scan(subs, cost_fun) do
+    # we scan from position 0 and up, stopping when the cost gradient turns positive.
+    # the assumption made here is that all subs are at non-negative position,
+    # so that position 0 has no subs on the left side
+    pos0_cost = cost_fun.(subs, 0)
+    search(0, pos0_cost, subs, cost_fun)
+  end
+
+  def run_a do
+    data = read_data("day7_input.txt")
+    lowest_cost = scan(data, &linear_cost_for_pos/2)
+    IO.puts(lowest_cost)
+  end
+
+  def run_b do
+    data = read_data("day7_input.txt")
+    lowest_cost = scan(data, &sum_cost_for_pos/2)
+    IO.puts(lowest_cost)
+  end
+
+end
+
+Day7.run_a()
+Day7.run_b()
